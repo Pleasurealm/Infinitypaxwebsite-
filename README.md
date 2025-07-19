e4b35a788109979c4db928905934611a3572f656
Pleasurealm <moses@pleasurealm.com>
2025-07-19 20:18:14 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# Infinitypax Website
+
+Website structure scaffold based on GitHub repo and Studio Lowrie PDF.
---
+++ index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import GridAnimation from '../components/GridAnimation';
+
+export default function Home() {
+  return (
+    <main>
+      <GridAnimation />
+    </main>
+  );
+}
